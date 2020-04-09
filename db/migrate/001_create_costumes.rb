27ba995a42f73bcea457ab416261a3d555168f3e
Rico Mitchell <ricomitch@icloud.com>
2020-04-09 16:53:45 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,16 +2,16 @@
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
 
-  # def change
-  #   create_table :costumes do |t|
-  #     t.string :name
-  #     t.string :size
-  #     t.string :image_url
-  #     t.float :price
-  #
-  #     t.timestamps null: false
-  #   end
-  # end
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
 end
 
 # By convention, the class name should match the part of the
@@ -19,5 +19,5 @@ end
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and
+# In this change method, create columns with the correct names and 
 # value types according to the spec
